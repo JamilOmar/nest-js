2c7c67981fd1ada1237ae862de7dc9a1256db1df
Jamil Omar <jfalconi63@hotmail.com>
2022-05-10 16:58:03 -0400

    feat: heroku support

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  const port = 3000;
+  const port = process.env.PORT;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
 }
